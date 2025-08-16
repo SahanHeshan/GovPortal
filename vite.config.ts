f5a595a2ee1316cca2901d532094493353e2a5dc
Viduruwan-55 <ypviduruwan55@gmail.com>
2025-08-16 12:12:45 +0530

    update vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+const __dirname = path.dirname(new URL(".", import.meta.url).pathname);
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -13,5 +15,11 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  preview: {
+    // Allow requests from your Azure Container App host
+    allowedHosts: [
+      "govconn-portal.ambitioustree-9332536f.eastasia.azurecontainerapps.io",
+    ],
   },
 }));
